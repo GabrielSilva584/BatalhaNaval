79236b8a97efb0d5d4b72699a4b5b9f7e8699954
Unknown <vibertelle@yahoo.com.br>
2016-09-21 15:49:43 -0300

    Mais imagens

---
+++ ChatJava/src/Jogo/Navios.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jogo;
+
+/**
+ *
+ * @author viber
+ */
+
+public class Navios {
+    private int tam;
+    private int ini[][];
+    boolean rotacao;
+    void pos(int tam, int ini[][], boolean rotacao){
+        
+    }
+}
